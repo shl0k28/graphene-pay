31bdd17489e4073bb2e75d6c7545efd6d5cb33e7
Shlok D. Mange <shlok_mange@protonmail.com>
2021-04-25 02:28:20 +0400

    Payment Modal Design

--- src/components/DummyCheckout.tsx
+++ src/components/DummyCheckout.tsx
@@ -5,6 +5,7 @@ import { BsArrowRight } from 'react-icons/bs'
 import { BiCreditCard, BiBitcoin } from 'react-icons/bi'
 import { FaEthereum } from 'react-icons/fa'
 import { SiBitcoin, SiXrp } from 'react-icons/si'
+import Portis from '../assets/portis.png'
 
 const buttonClass = `focus:outline-none w-full hover:shadow-md p-2 text-gray-100 flex items-center justify-center`
 
@@ -60,47 +61,77 @@ const iconClass = ` text-2xl hover:bg-gray-100 cursor-pointer p-2`
 
 const CryptexCheckout: React.FC = () => {
     const [paymentProvider, setPaymentProvider] = React.useState<string | null>(null)
-
-    return(
-        <div>
-            {
-                paymentProvider === null && <SelectPaymentProvider />
-            }
-        </div>
-    )
-}
-
-const SelectPaymentProvider: React.FC = () => {
     return(
         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
                 <div className="border rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {
+                        paymentProvider === null 
+                        ? <SelectPaymentProvider setPaymentProvider={setPaymentProvider}/>
+                        : <SelectWallet />
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+interface IPaymentProvider {
+    setPaymentProvider:  React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+const SelectPaymentProvider: React.FC<IPaymentProvider> = ({setPaymentProvider}) => {
+    return(
+        <div>
                     <div className="px-8 py-4">
                         <h1>{titleMsg}</h1>
                     </div>
                     <div className="px-8 py-4 flex items-center justify-between">
                         <div className={`${iconClass} text-yellow-400 `}>
                             <div className="flex justify-center">
-                                <SiBitcoin className="text-3xl text-center" name="BTC"/>
+                                <SiBitcoin 
+                                    className="text-3xl text-center" 
+                                    name="BTC"
+                                    onClick={() => setPaymentProvider("BTC")}
+                                />
                             </div>
                             <p>Bitcoin </p>
                         </div>
                         <div className={`${iconClass} text-gray-900`}>
                             <div className="flex justify-center">
-                                <FaEthereum className="text-3xl" name="ETH"/>
+                                <FaEthereum 
+                                    className="text-3xl" 
+                                    name="ETH"
+                                    onClick={() => setPaymentProvider("ETH")}
+                                />
                             </div>
                             <p>Ethereum </p>
                         </div>
                         <div className={`${iconClass} text-green-900`}>
                             <div className="flex justify-center">
-                                <SiXrp className="text-3xl" name="XRP"/>
+                                <SiXrp 
+                                    className="text-3xl" 
+                                    name="XRP"
+                                    onClick={() => setPaymentProvider("XRP")}
+                                />
                             </div>
                             <p>Ripple</p>
                         </div>
                     </div>
-                </div>
+        </div>
+    )
+}
+
+const SelectWallet: React.FC = () => {
+    return(
+        <div className="px-8 py-4">
+            <h1>Choose your preferred wallet</h1>
+            <div className="flex justify-center items-center space-y-2 hover:bg-gray-200 w-auto px-4 py-2 rounded-md cursor-pointer">
+                <img src={Portis} className="h-24" onClick={() => {}}/>
+                {/* <img src={Portis} className="h-24"/> */}
             </div>
         </div>
     )
 }
+
 export default DummyCheckout
