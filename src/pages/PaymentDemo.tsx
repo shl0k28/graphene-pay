34728e1850870f90ac077172af3205694bb3399f
Shlok D. Mange <shlok_mange@protonmail.com>
2021-06-03 01:09:17 +0400

    Demo Completed

--- src/pages/PaymentDemo.tsx
+++ src/pages/PaymentDemo.tsx
@@ -2,22 +2,26 @@ import { ethers } from 'ethers'
 import React from 'react'
 import { BiArrowBack } from 'react-icons/bi'
 import { FaAngleRight } from 'react-icons/fa'
+import { HiBadgeCheck } from 'react-icons/hi'
 import { NavLink } from 'react-router-dom'
 import metafox from '../assets/metamask_fox.png'
 import walletconnect from '../assets/walletconnect.svg'
+
+// @ts-ignore
+const globalEthereum = window.ethereum
+const merchantAddress = `0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266`
 
 const PaymentDemo: React.FC = () => {
 
     const [accountAddress, setAccountAddress] = React.useState<string | null>(null)
     const [accountBalance, setAccountBalance] = React.useState<string | null>(null)
     const [signer, setSigner] = React.useState<ethers.providers.JsonRpcSigner | null>(null)
+    const [isTransaction, setIsTransaction] = React.useState<boolean | null>(null)
+    const [ctx, setCtx] = React.useState<ethers.providers.TransactionResponse | null>(null)
 
     const merchantName = `Cryptify Technologies Inc.`
     const warning_message = `PLEASE NOTE THAT THIS GATEWAY IS NOT FOT TRANSFERRING REAL CRYPTO, KINDLY TEST IT WITH FAUCET CURRENCY ONLY
     `
-    const merchantAddress = `0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266`
-    // @ts-ignore
-    const globalEthereum = window.ethereum
     
     // gets the users metamask account
     const requestAccount = async () => {
@@ -41,14 +45,16 @@ const PaymentDemo: React.FC = () => {
         }
     }
 
-    const confirmPayment = async (signer: ethers.providers.JsonRpcSigner) => {
+    const confirmPayment = async () => {
+        // @ts-expect-error
         const ctx = await signer.sendTransaction({
             to: merchantAddress,
             value: ethers.utils.parseEther("0.3")
         })
+        setCtx(ctx)
         console.log(ctx)
     }
-    
+
     return(
         <div className="bg-gray-100 h-screen w-screen overflow-auto" style={{fontFamily:"'Rubik', sans-serif"}}>
             {/* Top Bar */}
@@ -62,17 +68,91 @@ const PaymentDemo: React.FC = () => {
                 <div className="flex justify-center">
                     {/*  Orderbook Card Component */}
                     <div className="w-full border max-w-md bg-white rounded-md shadow-md space-y-2">
-                        <div className="bg-gray-900 text-gray-100 w-full rounded-md rounded-b-none py-2 px-4">
-                            <h2 className="text-center text-gray-500 text-lg">You are paying to</h2>
+                        <div className="bg-gray-800 text-gray-50 w-full rounded-md rounded-b-none py-2 px-4">
+                            <h2 className="text-center text-gray-100 text-lg">You are paying to</h2>
                             <h1 className="text-center text-2xl">{merchantName}</h1>
                         </div>
                         <div className="py-2 px-4 space-y-4">
                             <div>
                                 <p className="text-center text-gray-600">Order Total</p>
-                                <h2 className="text-center text-indigo-800 font-semibold text-2xl">$39.99</h2>
+                                <h2 className="text-center text-gray-800 font-semibold text-2xl">$39.99</h2>
                             </div>
-                            <h1 className="text-center text-gray-800 text-xl">Choose your Wallet</h1>
-                            <div className="rounded">
+                            <div>
+                            {
+                                isTransaction && <TransactionDetails ctx={ctx}/>
+                            }
+                            {
+                                accountAddress && accountBalance && !isTransaction ? 
+                                <MyAccount 
+                                    accountAddress={accountAddress}
+                                    accountBalance={accountBalance}
+                                    confirmPayment={confirmPayment}
+                                    setIsTransaction={setIsTransaction}
+                                /> : (
+                                    !isTransaction ?
+                                    <ChooseWallet 
+                                        requestAccount={requestAccount}
+                                        transferEth={transferEth}
+                                    /> : <></>
+                                ) 
+                            }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <p className="text-red-500 text-center">** {warning_message} **</p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+interface IAccount {
+    accountAddress: string ,
+    accountBalance: string ,
+    confirmPayment: () => {},
+    setIsTransaction: React.Dispatch<React.SetStateAction<boolean | null>>
+}
+
+const MyAccount: React.FC<IAccount> = ({
+    accountAddress,
+    accountBalance,
+    confirmPayment,
+    setIsTransaction
+}) => {
+    return(
+        <div className="text-gray-700 space-y-4">
+            <div className="px-4 space-y-1 border-b">
+            <p className="flex justify-between items-center">Cost in $USD <span className="font-medium text-xl text-gray-900">39.99</span></p>
+                <p className="flex justify-between items-center">Cost in ETH <span className="font-medium text-xl text-gray-900">0.298</span></p>
+                <p className="flex justify-between items-center">Your Balance <span className="font-medium text-xl text-gray-900">{accountBalance.slice(0,6)}</span></p>
+            </div>
+            <div className="px-4">
+                <button
+                    onClick={async () => {
+                        await confirmPayment()
+                        setIsTransaction(true)
+                    }} 
+                    className="p-2 w-full bg-blue-800 text-white rounded-md shadow-md hover:bg-blue-500 transition ease-in duration-200"
+                >
+                    Confirm Payment
+                </button>
+            </div>
+        </div>
+    )
+}
+
+interface IWallet {
+    requestAccount: () => {},
+    transferEth: () => {}
+}
+
+const ChooseWallet: React.FC<IWallet> = ({requestAccount, transferEth}) => {
+    return(
+        <div>
+            <h1 className="text-center text-gray-800 text-xl">Choose your Wallet</h1>
+                <div className="rounded">
                                 <div className="border md:p-4 py-2 px-4 flex items-center space-x-4 hover:bg-blue-100">
                                     <div className="bg-yellow-100 rounded-full lg:h-16 h-12 w-12 lg:w-16 flex justify-center items-center">
                                         <img src={metafox} alt="" className="h-10 w-10"/>
@@ -104,24 +184,35 @@ const PaymentDemo: React.FC = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
                 </div>
-                <div>
-                    <p className="text-red-500 text-center">** {warning_message} **</p>
+    )
+}
+
+interface ICtx {
+    ctx: ethers.providers.TransactionResponse | null
+}
+
+const TransactionDetails: React.FC<ICtx> = ({
+    ctx
+}) => {
+
+    const url = `https://explorer-mumbai.maticvigil.com/tx/${ctx?.hash}/internal-transactions`
+    if(ctx?.hash){
+        return(
+            <div>
+                <p className="text-green-400 flex items-center justify-center text-xl">
+                    <HiBadgeCheck />
+                    SUCCESS
+                </p>
+                <p className="text-center">You payment was successfully processed.</p>
+                <div className="flex justify-center">
+                    <a href={url} target="_blank" rel="noopener" className="text-center text-blue-600 underline">Verify It here.</a>
                 </div>
-                {
-                    accountAddress && 
-                    <div className="flex justify-center">
-                        <div>
-                            <p>Your account: {accountAddress}</p>
-                            <p className="text-center">{accountBalance}</p>
-                            <button className="p-2 w-full bg-gray-900 text-white">Pay</button>
-                        </div>
-                    </div>
-                }
-            </section>
-        </div>
+            </div>
+        )
+    }
+    return(
+        <></>
     )
 }
 
