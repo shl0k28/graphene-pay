c06116a9eb82d6aeaccb65d590ea832a507b0192
Shlok D. Mange <shlok_mange@protonmail.com>
2021-06-02 13:07:12 +0400

    Chaged the landing page

--- src/pages/PaymentDemo.tsx
+++ src/pages/PaymentDemo.tsx
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers'
 import React from 'react'
 import { BiArrowBack } from 'react-icons/bi'
 import { FaAngleRight } from 'react-icons/fa'
@@ -8,6 +9,26 @@ import walletconnect from '../assets/walletconnect.svg'
 const PaymentDemo: React.FC = () => {
 
     const merchantName = `Cryptify Technologies Inc.`
+    const warning_message = `PLEASE NOTE THAT THIS GATEWAY IS NOT FOT TRANSFERRING REAL CRYPTO, KINDLY TEST IT WITH FAUCET CURRENCY ONLY
+    `
+    // @ts-ignore
+    const globalEthereum = window.ethereum
+    
+    // gets the users metamask account
+    const requestAccount = async () => {
+        await globalEthereum.request({ method: 'eth_requestAccounts' });
+    }
+
+    const transferEth = async () => {
+        if(typeof globalEthereum !== 'undefined'){
+            await requestAccount()
+            const provider = new ethers.providers.Web3Provider(globalEthereum)
+            const signer = provider.getSigner()
+            const accountAddress = await signer.getAddress()
+            const accountBalance = await signer.getBalance()
+            console.log(`Address: `, accountAddress, `Balance:`, ethers.utils.formatUnits(accountBalance, 18))
+        }
+    }
 
     return(
         <div className="bg-gray-100 h-screen w-screen overflow-auto" style={{fontFamily:"'Rubik', sans-serif"}}>
@@ -18,7 +39,7 @@ const PaymentDemo: React.FC = () => {
                 </NavLink>
             </nav>
             {/* Payments Section */}
-            <section className="px-8 py-4">
+            <section className="px-8 py-4 space-y-4">
                 <div className="flex justify-center">
                     {/*  Orderbook Card Component */}
                     <div className="w-full border max-w-md bg-white rounded-md shadow-md space-y-2">
@@ -37,7 +58,7 @@ const PaymentDemo: React.FC = () => {
                                     <div className="bg-yellow-100 rounded-full lg:h-16 h-12 w-12 lg:w-16 flex justify-center items-center">
                                         <img src={metafox} alt="" className="h-10 w-10"/>
                                     </div>
-                                    <button className="flex items-center group-hover:bg-opacity-5">
+                                    <button onClick={transferEth} className="flex items-center group-hover:bg-opacity-5">
                                         <div>
                                             <p className="font-bold text-left">MetaMask</p>
                                             <p className="text-gray-500 hidden md:block">Connect with a browser wallet</p>
@@ -55,7 +76,7 @@ const PaymentDemo: React.FC = () => {
                                         <button className="flex items-center">
                                             <div>
                                                 <p className="font-bold text-left">WalletConnect</p>
-                                                <p className="text-gray-500 hidden md:block">Connect with a browser wallet</p>
+                                                <p className="text-gray-500 hidden md:block">Connect with your mobile wallet</p>
                                             </div>
                                             <div className="ml-8 hover:text-blue-600">
                                                 <FaAngleRight className="text-2xl"/>
@@ -66,6 +87,9 @@ const PaymentDemo: React.FC = () => {
                             </div>
                         </div>
                     </div>
+                </div>
+                <div>
+                    <p className="text-red-500 text-center">** {warning_message} **</p>
                 </div>
             </section>
         </div>
